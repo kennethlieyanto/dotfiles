16620f14925d75abb9319660a97dca4e391ee5d3
Kenneth Manuel Lieyanto <kenneth.lieyanto@suryapamenang.com>
2025-03-26 09:58:03 +0700

    powershell profile (windows)

---
+++ readonly_Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+function Run-DotnetWatchRun {
+    dotnet watch run
+}
+
+function Run-DotnetRun {
+    dotnet run
+}
+
+function Run-GitStatus {
+    git status
+}
+
+function Edit-Hostfile {
+    notepad c:\Windows\System32\Drivers\etc\hosts
+}
+
+function Run-GhDash {
+    gh dash
+}
+
+New-Alias -Name lzg -Value lazygit
+New-Alias -Name lzd -Value lazydocker
+New-Alias -Name dwr -Value Run-DotnetWatchrun
+New-Alias -Name dr -Value Run-DotnetRun
+New-Alias -Name o -Value start
+New-Alias -Name ghd -Value Run-GhDash
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Invoke-Expression (&starship init powershell)
+
+if ( Test-Path '~/.inshellisense/powershell/init.ps1' -PathType Leaf ) { . ~/.inshellisense/powershell/init.ps1 }
