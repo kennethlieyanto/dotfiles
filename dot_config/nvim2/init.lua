8fad8e4606b9cb8c00a5ae2ec47e2d5516820fac
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-05-06 07:28:59 +0700

    nvim from scratch

---
+++ dot_config/nvim2/init.lua
@@ -0,0 +1,20 @@
+require("config.lazy")
+require("config.keymaps")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source <CR>%")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text', 
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
---
+++ dot_config/nvim2/lua/config/keymaps.lua
@@ -0,0 +1,55 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "paste without dirtying current register" })
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "yank to plus register" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "yank plus register" })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "delete to plus register" })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "delete to plus register" })
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<C-q>", "<cmd>only<CR>")
+
+if vim.g.vscode then
+  vim.keymap.set("n", "<leader>`", "<Cmd>lua require('vscode').call('vscode-harpoon.addEditor')<CR>")
+  vim.keymap.set("n", "<leader>1", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor1')<CR>")
+  vim.keymap.set("n", "<leader>2", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor2')<CR>")
+  vim.keymap.set("n", "<leader>3", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor3')<CR>")
+  vim.keymap.set("n", "<leader>4", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor4')<CR>")
+  vim.keymap.set("n", "<leader>5", "<Cmd>lua require('vscode').call('vscode-harpoon.gotoEditor5')<CR>")
+  vim.keymap.set("n", "<leader>hh", "<Cmd>lua require('vscode').call('vscode-harpoon.editEditors')<CR>")
+
+  vim.keymap.set("n", "<leader>we", "<Cmd>lua require('vscode').call('workbench.view.explorer')<CR>")
+  vim.keymap.set("n", "<leader>wt", "<Cmd>lua require('vscode').call('workbench.view.testing.focus')<CR>")
+  vim.keymap.set(
+    "n",
+    "<leader>wx",
+    "<Cmd>lua require('vscode').call('workbench.extensions.action.focusExtensionsView')<CR>"
+  )
+  vim.keymap.set("n", "<leader>wq", "<Cmd>lua require('vscode').call('workbench.panel.markers.view.focus')<CR>")
+
+  vim.keymap.set("n", "<leader>ta", "<Cmd>lua require('vscode').call('testing.runAll')<CR>")
+  vim.keymap.set("n", "<leader>tf", "<Cmd>lua require('vscode').call('testing.reRunFailTests')<CR>")
+  vim.keymap.set("n", "<leader>tr", "<Cmd>lua require('vscode').call('testing.runAtCursor')<CR>")
+
+  vim.keymap.set("n", "]q", "<Cmd>lua require('vscode').call('editor.action.marker.next')<CR>")
+  vim.keymap.set("n", "[q", "<Cmd>lua require('vscode').call('editor.action.marker.next')<CR>")
+
+  vim.keymap.set("n", "<leader>ss", "<Cmd>lua require('vscode').call('workbench.action.showAllSymbols')<CR>")
+
+  vim.keymap.set("n", "<leader>F", "<Cmd>lua require('vscode').call('workbench.action.toggleZenMode')<CR>")
+
+  vim.keymap.set("n", "<leader>ca", "<Cmd>lua require('vscode').call('editor.action.quickfix')<CR>")
+
+  vim.keymap.set("n", "<leader>llm", "<Cmd>lua require('vscode').call('composer.newAgentChat')<CR>")
+else
+end
+
