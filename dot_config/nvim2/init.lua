bd9d6648a8de971c30ba5c3528a870e2d9273a99
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-05-06 23:19:10 +0700

    vim config

--- dot_config/nvim2/init.lua
+++ dot_config/nvim2/init.lua
@@ -11,7 +11,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text', 
+  desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
