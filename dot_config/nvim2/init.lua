e5fc32074baf4138bb29453e4594c8202482d628
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-06-08 22:29:17 +0700

    nvim: lsp use mason

--- dot_config/nvim2/init.lua
+++ dot_config/nvim2/init.lua
@@ -20,28 +20,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-function Open_lazygit()
-  local buf = vim.api.nvim_create_buf(false, true)
-  local width = math.floor(vim.o.columns * 0.85)
-  local height = math.floor(vim.o.lines * 0.85)
-  local row = math.floor((vim.o.lines - height) / 2)
-  local col = math.floor((vim.o.columns - width) / 2)
-
-  local win = vim.api.nvim_open_win(buf, true, {
-    relative = "editor",
-    width = width,
-    height = height,
-    row = row,
-    col = col,
-    style = "minimal",
-    border = "rounded",
-  })
-
-  vim.fn.termopen("lazygit", { on_exit = function()
-    vim.api.nvim_win_close(win, true)
-  end })
-  vim.cmd("startinsert")
-end
-
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Open_lazygit()<CR>", { noremap = true, silent = true })
-
