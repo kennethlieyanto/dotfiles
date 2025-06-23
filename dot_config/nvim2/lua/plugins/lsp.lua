1e387efa72f745272b702613e9ce79f70ccde5a0
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-06-24 00:10:08 +0700

    trouble / lsp diagnostics quickfix

--- dot_config/nvim2/lua/plugins/lsp.lua
+++ dot_config/nvim2/lua/plugins/lsp.lua
@@ -31,5 +31,42 @@ return {
     config = function()
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>wQ",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>wq",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      -- {
+      --   "<leader>cs",
+      --   "<cmd>Trouble symbols toggle focus=false<cr>",
+      --   desc = "Symbols (Trouble)",
+      -- },
+      -- {
+      --   "<leader>cl",
+      --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      --   desc = "LSP Definitions / references / ... (Trouble)",
+      -- },
+      -- {
+      --   "<leader>xL",
+      --   "<cmd>Trouble loclist toggle<cr>",
+      --   desc = "Location List (Trouble)",
+      -- },
+      {
+        "<leader>wf",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   }
 }
