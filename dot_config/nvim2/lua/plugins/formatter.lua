22faea3abc81356ea98b5d318e835f654b89a872
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-08-16 10:32:05 +0700

    ide: neovim .NET development related config

--- dot_config/nvim2/lua/plugins/formatter.lua
+++ dot_config/nvim2/lua/plugins/formatter.lua
@@ -4,7 +4,6 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        cs = { "csharpier" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
@@ -17,33 +16,17 @@ return {
         timeout_ms = 500,
         lsp_format = "fallback",
       },
-      formatters = {
-        csharpier = function()
-          local useDotnet = not vim.fn.executable("csharpier")
-
-          local command = useDotnet and "dotnet csharpier" or "csharpier"
-
-          local version_out = vim.fn.system(command .. " --version")
-
-          vim.notify(version_out)
-
-          --NOTE: system command returns the command as the first line of the result, need to get the version number on the final line
-          local version_result = version_out[#version_out]
-          local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
-          local is_new = major_version >= 1
-
-          vim.notify(tostring(is_new))
-
-          local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
-
-          return {
-            command = command,
-            args = args,
-            stdin = not is_new,
-            require_cwd = false,
-          }
-        end,
-      },
     })
   end,
+  opts = {
+    formatters_by_ft = {
+      cs = { "csharpier" },
+    },
+    formatters = {
+      csharpier = {
+        command = "dotnet-csharpier",
+        args = { "--write-stdout" },
+      },
+    },
+  },
 }
