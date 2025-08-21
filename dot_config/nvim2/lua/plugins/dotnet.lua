11f41a7c08a738f410d41e051b610b94ed6074df
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2025-08-21 09:56:44 +0700

    ide: neovim .NET dev setup

--- dot_config/nvim2/lua/plugins/dotnet.lua
+++ dot_config/nvim2/lua/plugins/dotnet.lua
@@ -2,6 +2,18 @@ return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
-    require("easy-dotnet").setup()
+    local dotnet = require("easy-dotnet")
+
+    dotnet.setup({
+      auto_bootstrap_namespace = {
+        --block_scoped, file_scoped
+        type = "block_scoped",
+        enabled = true,
+        use_clipboard_json = {
+          behavior = "prompt", --'auto' | 'prompt' | 'never',
+          register = "+",      -- which register to check
+        },
+      },
+    })
   end,
 }
