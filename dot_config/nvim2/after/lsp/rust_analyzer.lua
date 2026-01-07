1a3b4d10c9a588a2783aa42bc8d59747ea8e6aee
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-01-07 23:24:52 +0700

    disable typehint on insert mode

--- dot_config/nvim2/after/lsp/rust_analyzer.lua
+++ dot_config/nvim2/after/lsp/rust_analyzer.lua
@@ -1,6 +1,9 @@
 return {
   on_attach = function(_, bufnr)
     print("rust_analyzer attached! bufnr" .. bufnr)
-    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    -- Inlay hints are now controlled by autocmds based on mode
+    if vim.fn.mode() == "n" then
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end
   end,
 }
--- dot_config/nvim2/lua/config/autocmds.lua
+++ dot_config/nvim2/lua/config/autocmds.lua
@@ -5,3 +5,30 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- Enable inlay hints only in normal mode
+local inlay_hint_group = vim.api.nvim_create_augroup("InlayHintsModeControl", { clear = true })
+
+local function toggle_inlay_hints()
+  local mode = vim.fn.mode()
+  local enabled = mode == "n" or mode == "v" or mode == "V" or mode == "" or mode == "s" or mode == "S" or mode == ""
+
+  vim.lsp.inlay_hint.enable(enabled, { bufnr = 0 })
+end
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
+  group = inlay_hint_group,
+  callback = function()
+    vim.lsp.inlay_hint.enable(false, { bufnr = 0 })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "CmdlineLeave", "ModeChanged" }, {
+  group = inlay_hint_group,
+  callback = function()
+    local mode = vim.fn.mode()
+    if mode == "n" or mode == "v" or mode == "V" or mode == "" or mode == "s" or mode == "S" or mode == "" then
+      vim.lsp.inlay_hint.enable(true, { bufnr = 0 })
+    end
+  end,
+})
