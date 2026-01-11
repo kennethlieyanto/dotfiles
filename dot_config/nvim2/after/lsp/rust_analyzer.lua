50d9b1142099620756edee63f36484977baf93a0
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-01-11 13:01:42 +0700

    inlay hint settings

--- dot_config/nvim2/after/lsp/rust_analyzer.lua
+++ dot_config/nvim2/after/lsp/rust_analyzer.lua
@@ -1,9 +1,5 @@
 return {
 	on_attach = function(_, bufnr)
-		print("rust_analyzer attached! bufnr" .. bufnr)
-		-- Inlay hints are now controlled by autocmds based on mode
-		if vim.fn.mode() == "n" then
-			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-		end
+		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 	end,
 }
