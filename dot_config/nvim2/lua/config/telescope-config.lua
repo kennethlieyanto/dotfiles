ea48a3267642bb4edebed45a8571cbbd11c55bb5
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2026-02-10 22:38:51 +0700

    nvim better fallback searching show git files include untracked

--- dot_config/nvim2/lua/config/telescope-config.lua
+++ dot_config/nvim2/lua/config/telescope-config.lua
@@ -3,8 +3,6 @@ local M = {}
 local is_inside_work_tree = {}
 
 M.project_files = function()
-	local opts = {}
-
 	local cwd = vim.fn.getcwd()
 	if is_inside_work_tree[cwd] == nil then
 		vim.fn.system("git rev-parse --is-inside-work-tree")
@@ -12,9 +10,11 @@ M.project_files = function()
 	end
 
 	if is_inside_work_tree[cwd] then
-		require("telescope.builtin").git_files(opts)
+		require("telescope.builtin").git_files({
+			show_untracked = true,
+		})
 	else
-		require("telescope.builtin").find_files(opts)
+		require("telescope.builtin").find_files({})
 	end
 end
 
--- dot_config/nvim2/lua/plugins/telescope.lua
+++ dot_config/nvim2/lua/plugins/telescope.lua
@@ -11,22 +11,22 @@ return {
 			-- Core
 			vim.keymap.set("n", "<C-p>", "<CMD>lua require'config.telescope-config'.project_files()<CR>")
 			vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader>fs", builtin.lsp_dynamic_workspace_symbols)
-			vim.keymap.set("n", "<leader>fr", builtin.oldfiles)
+			vim.keymap.set("n", "<leader>fs", builtin.lsp_dynamic_workspace_symbols, { desc = "Find symbols" })
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent files" })
 
 			vim.keymap.set("n", "<leader>ff", function()
 				builtin.find_files({ hidden = true })
-			end)
+			end, { desc = "Find files including hidden ones" })
 
 			-- VIM specific
 			vim.keymap.set("n", "<leader>fc", function()
 				builtin.find_files({
 					cwd = vim.fn.stdpath("config"),
 				})
-			end)
-			vim.keymap.set("n", "<leader>fH", builtin.help_tags)
-			vim.keymap.set("n", "<leader>fC", builtin.commands)
-			vim.keymap.set("n", "<leader>fK", builtin.keymaps)
+			end, { desc = "Find configuration files" })
+			vim.keymap.set("n", "<leader>fH", builtin.help_tags, { desc = "Find help" })
+			vim.keymap.set("n", "<leader>fC", builtin.commands, { desc = "Find config" })
+			vim.keymap.set("n", "<leader>fK", builtin.keymaps, { desc = "Find keymaps" })
 		end,
 	},
 }
